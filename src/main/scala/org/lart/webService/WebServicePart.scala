c5a73caea00970ab83f8d843a3898d0a697da241
Leonid Artioukhov <LArtioukhov@users.noreply.github.com>
2017-11-10 12:43:30 +0300

    Delete WebServicePart.scala

--- src/main/scala/org/lart/webService/WebServicePart.scala
+++
@@ -1,8 +0,0 @@
-package org.lart.webService
-
-import akka.http.scaladsl.server.{Directives, Route}
-
-trait WebServicePart extends Directives {
-  lazy val route: Route = RouteGenerator
-  def RouteGenerator: Route
-}
